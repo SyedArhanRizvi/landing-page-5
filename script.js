2ee52ba046d3ac5061b5eb6cc246d92c85de5579
Syed Arhan Rizvi <arhanrizvi9@gmail.com>
2025-06-20 04:53:59 +0530

    Testing Scrolling Event

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-const main = document.getElementById("mainContainer");
+// console.log("top log");
 
-main.addEventListener("scroll", () => {
-  const scrollTop = main.scrollTop;
-  const scrollHeight = main.scrollHeight;
-  const offsetHeight = main.offsetHeight;
+// let lastScrollY = window.scrollY;
+// const navbar = document.getElementById("navbar");
+// const menuBtn = document.getElementById("menu-btn");
+// const mobileMenu = document.getElementById("mobile-menu");
 
-  if (scrollTop + offsetHeight >= scrollHeight - 10) {
-    // Reached end
-    main.classList.add("!-top-[100%]");
-  } else {
-    // Not at end
-    main.classList.remove("!-top-[100%]");
-  }
-});
+// window.addEventListener("scroll", () => {
+//   console.log("top log");
+//   debugger
+//   console.log("heelo");
+  
+//   if (window.scrollY > lastScrollY) {
+//     // Scrolling down
+//     navbar.style.top = "-100%";
+//     mobileMenu.classList.add("hidden");
+//   } else {
+//     // Scrolling up
+//     navbar.style.top = "20px"; // Or match with your Tailwind class e.g., `top-5`
+//   }
+
+//   lastScrollY = window.scrollY;
+// });
+
+window.addEventListener('scroll', (e)=>{
+  console.log(e);
+  
+})
